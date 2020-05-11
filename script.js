5be87e62c4258f8665b131774415d0222881db8a
imstylen <michael.steijlen@gmail.com>
2020-05-11 11:35:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 window.onload = function() {
-     function pButton() {
-          alert('click!');
-          document.getElementById("punishment").innerHTML = "red";
-     }
 
+     function pButton() {
+          
+          var punishments =
+          [
+               "Go to world 302 and spam buying combat bracelet until someone sells you one.",
+               "Kill Zulrah with a setup costing less that 500k total. If you fail, roll again.",
+               "Change character to a female for 24 hours.",
+               "Trade a level 126 a combat bracelet and say 'You look like you can use this..'",
+               "Buy an inventory of cowhides in lumbridge, then drop them.",
+               "Ask someone at barrows what the drop rate of barrows gloves is.",
+               "Fish for 10 minutes.",
+               "Mine for 10 minutes.",
+               "Go to chaos dwarves in a populated world. Kill one and say 'sit'.",
+               "Ask someone with a pet when summoning was released.",
+          ]
+
+          var randomPunishment = punishments[Math.floor(Math.random()*punishments.length)];
+
+          alert('click!');
+          document.getElementById("punishment").innerHTML = randomPunishment;
+     }
 
      document.getElementById ("punishment-button").addEventListener ("click", pButton, false);
 
