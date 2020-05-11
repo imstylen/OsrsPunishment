98bd5ffd6079538f5f5c85d92f3d725544c3905a
imstylen <michael.steijlen@gmail.com>
2020-05-11 11:40:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.onload = function() {
           [
                "Go to world 302 and spam buying combat bracelet until someone sells you one.",
                "Kill Zulrah with a setup costing less that 500k total. If you fail, roll again.",
+               "Kill Vorkath with a setup costing less that 500k total. If you fail, roll again.",
                "Change character to a female for 24 hours.",
                "Trade a level 126 a combat bracelet and say 'You look like you can use this..'",
                "Buy an inventory of cowhides in lumbridge, then drop them.",
