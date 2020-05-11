c2e6c69b441afaf0318878cec6b9fa5383d41591
imstylen <michael.steijlen@gmail.com>
2020-05-11 11:08:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-
-window.onLoad = function()
-{
-     document.getElementById("punishment-button").onClick = function pButton()
-     {
-          alert('click!');
-          document.getElementById("punishment").innerHTML = "red";
-     }
+document.getElementById("punishment-button").onClick = function pButton() {
+     alert('click!');
+     document.getElementById("punishment").innerHTML = "red";
 }
